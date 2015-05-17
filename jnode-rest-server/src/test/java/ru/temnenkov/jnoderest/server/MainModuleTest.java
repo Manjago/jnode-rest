2c8b982dc044c17da84f31f30370b0c74f39a0d4
Kirill Temnenkov <kirill@temnenkov.com>
2015-05-17 21:14:30 +0300

    нормальный тест для спарка

--- jnode-rest-server/src/test/java/ru/temnenkov/jnoderest/server/MainModuleTest.java
+++ jnode-rest-server/src/test/java/ru/temnenkov/jnoderest/server/MainModuleTest.java
@@ -2,22 +2,35 @@ package ru.temnenkov.jnoderest.server;
 
 import jnode.module.JnodeModuleException;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
 
 public class MainModuleTest {
 
+    private static MainModule mainModule;
+
+    @BeforeClass
+    public static void setUpTotal() throws Exception {
+        mainModule = new MainModule(MainModule.class.getResource("config.properties").getPath());
+        mainModule.start();
+    }
 
     @Test
     public void testPort() throws Exception {
-        MainModule mainModule = new MainModule(getClass().getResource("config.properties").getPath());
         Assert.assertEquals(7888, mainModule.getPort());
-
     }
 
     @Test
     public void testStart() throws Exception {
-        MainModule mainModule = new MainModule(getClass().getResource("config.properties").getPath());
-        mainModule.start();
+        Thread.sleep(1000);
+    }
 
+    @Test
+    public void testStart2() throws Exception {
+        Thread.sleep(1000);
     }
 }
